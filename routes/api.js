4425b6d762b9726dcaa18fd8b73ce3f8d6af876f
Chen Jian <lsdlab@icloud.com>
2016-07-30 00:13:44 +0800

    update

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
+
 var url = 'mongodb://localhost:27017/spending-vis-backend';
 
 var _ = require('underscore');
