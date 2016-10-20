c825e6759757ae38e410951f14ed17e0598db417
Chen Jian <lsdlab@icloud.com>
2016-10-21 00:18:57 +0800

    some fix

--- app.js
+++ app.js
@@ -11,36 +11,37 @@ const apiRouter = require('./routes/api')
 
 // dependencies
 const dotenv = require('dotenv')
-const chalk = require('chalk');
-const flash = require('express-flash')
-const lusca = require('lusca')
+const chalk = require('chalk')
+const expressValidator = require('express-validator')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const MongoStore = require('connect-mongo/es5')(session)
+const lusca = require('lusca')
+const flash = require('express-flash')
 const passport = require('passport')
-const expressValidator = require('express-validator')
 const multer = require('multer')
 const upload = multer({ dest: path.join(__dirname, 'uploads') })
+require('nodejs-dashboard')
 
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
 dotenv.load({
-    path: '.env.development'
+  path: '.env.development'
 })
 
 /**
  * mongoose connect to MongoDB.
  */
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on('connected', () => {
-  console.log('%s MongoDB connection established!', chalk.blue('✓'));
-});
+  console.log('%s MongoDB connection established!', chalk.blue('✓'))
+})
 mongoose.connection.on('error', () => {
-  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
-  process.exit();
-});
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'))
+  process.exit()
+})
 
 
 const app = express()
@@ -48,10 +49,10 @@ const app = express()
 /**
  * Express configuration.
  */
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000)
 // swig template engine setup
 const swig = require('swig')
-// This is where all the magic happens!
+  // This is where all the magic happens!
 app.engine('html', swig.renderFile)
 
 app.set('view engine', 'html')
@@ -60,9 +61,9 @@ app.set('views', __dirname + '/views')
 // Swig will cache templates for you, but you can disable
 // that and use Express's caching instead, if you like:
 app.set('view cache', false)
-// To disable Swig's cache, do the following:
+  // To disable Swig's cache, do the following:
 swig.setDefaults({
-    cache: false
+  cache: false
 })
 // NOTE: You should always cache templates in a production environment.
 // Don't leave both of these to `false` in production!
@@ -70,13 +71,16 @@ swig.setDefaults({
 // express status monitor
 app.use(require('express-status-monitor')())
 
+// nodejs-dashboard
+require('nodejs-dashboard')
+
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public/img/', 'favicon.png')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(expressValidator())
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
@@ -84,37 +88,43 @@ app.use(express.static(path.join(__dirname, 'public')))
 // dependencies setup
 app.use(expressValidator())
 app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: process.env.SESSION_SECRET,
-    store: new MongoStore({
-        url: process.env.MONGODB_URI,
-        autoReconnect: true
-    })
+  secret: process.env.MONGODB_SECRET,
+  saveUninitialized: true,
+  resave: true,
+  store: new MongoStore({
+    url: process.env.MONGODB_URI,
+    ttl: 7 * 24 * 60 * 60,
+    autoReconnect: true,
+    autoRemove: 'native'
+  })
 }))
+
+app.use(lusca({
+  xframe: 'SAMEORIGIN',
+  xssProtection: true,
+  nosniff: true
+}))
+app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(flash())
-app.use(lusca.xframe('SAMEORIGIN'))
-app.use(lusca.xssProtection(true))
 app.use(function(req, res, next) {
-    res.locals.user = req.user
-    next()
+  res.locals.user = req.user
+  app.locals._ = require('underscore')
+  next()
 })
-app.locals._ = require('underscore')
 
 app.use(function(req, res, next) {
-    // After successful login, redirect back to the intended page
-    if (!req.user &&
-        req.path !== '/login' &&
-        req.path !== '/signup' &&
-        !req.path.match(/^\/auth/) &&
-        !req.path.match(/\./)) {
-        req.session.returnTo = req.path
-    }
-    next()
+  // After successful login, redirect back to the intended page
+  if (!req.user &&
+    req.path !== '/login' &&
+    req.path !== '/signup' &&
+    !req.path.match(/^\/auth/) &&
+    !req.path.match(/\./)) {
+    req.session.returnTo = req.path
+  }
+  next()
 })
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
+app.post('/profile', upload.single('avatar'), function(req, res, next) {
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
 })
@@ -125,9 +135,9 @@ app.use('/api', apiRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    const err = new Error('Not Found')
-    err.status = 404
-    next(err)
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
 })
 
 // error handlers
@@ -135,30 +145,30 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500)
-        res.render('error', {
-            message: err.message,
-            error: err
-        })
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
     })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500)
-    res.render('error', {
-        message: err.message,
-        error: {}
-    })
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
 })
 
 /**
  * Start Express server.
  */
 app.listen(app.get('port'), () => {
-  console.log('%s Express server listening on port %d in %s mode.', chalk.blue('✓'), app.get('port'), app.get('env'));
-});
+  console.log('%s Express server listening on port %d in %s mode.', chalk.blue('✓'), app.get('port'), app.get('env'))
+})
 
-module.exports = app;
+module.exports = app
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
-
-const User = require('../models/user')
 const passport = require('passport')
+const shortid = require('shortid')
+const User = require('../models/user')
 const passportConfig = require('../passport/passport')
+
+const dotenv = require('dotenv')
+dotenv.load({
+  path: '.env.development'
+})
+
+const postmark = require('../email_utils/postmark')
 
 
 /**
@@ -12,12 +19,10 @@ const passportConfig = require('../passport/passport')
  */
 router.get('/signup', function(req, res) {
   if (req.user) {
-    req.flash('success', { msg: 'Signup successed' })
+    req.flash('success', { msg: '注册成功' })
     return res.redirect('/')
   }
-  res.render('user/signup', {
-    title: 'signup'
-  })
+  res.render('user/signup')
 })
 
 
@@ -39,26 +44,64 @@ router.post('/signup', function(req, res, next) {
 
   var user = new User({
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
+    accountStatus: 'verifying',
+    quickLoginToken: shortid.generate()
   })
 
   User.findOne({ email: req.body.email }, function(err, existingUser) {
     if (existingUser) {
-      req.flash('errors', { msg: 'Account with that email address already exists.' })
-      return res.redirect('/signup')
+      req.flash('errors', { msg: '此邮箱已经存在，请直接登录' })
+      var string = encodeURIComponent('verifying')
+      return res.redirect('/verifying?valid=' + string)
     }
+
     user.save(function(err) {
       if (err) {
         return next(err)
       }
-      req.logIn(user, function(err) {
+      // postmark.sendConfirmEmail(req.body.email, req.body.email, req.body.email,
+      //   req.headers.host + '/verifybyemail/' + user.quickLoginToken)
+      var string = encodeURIComponent('verifying')
+      var quicklogintoken = encodeURIComponent(user.quickLoginToken)
+      return res.redirect('/verifying?valid=' + string + '&quicklogintoken=' + quicklogintoken)
+    })
+  })
+})
+
+
+/**
+ * GET /verifying
+ * Verifying page.
+ */
+router.get('/verifying', function(req, res) {
+  var passedVariable = req.query.valid
+  var quicklogintoken = req.query.quicklogintoken
+  if (passedVariable == 'verifying') {
+    res.render('user/verifying', { quicklogintoken: quicklogintoken })
+  } else {
+    return res.redirect('/signup')
+  }
+})
+
+
+/**
+ * GET /verifybyemail
+ * Verify by email page.
+ */
+router.get('/verifybyemail/:quicklogintoken', function(req, res, next) {
+  User.findOne({ quickLoginToken: req.params.quicklogintoken }, function(err, existingUser) {
+    if (existingUser) {
+      req.logIn(existingUser, function(err) {
         if (err) {
           return next(err)
         }
-        req.flash('success', { msg: 'Create account successed!' })
         return res.redirect('/signup')
       })
-    })
+    } else {
+      req.flash('errors', err)
+      return res.redirect('/signup')
+    }
   })
 })
 
@@ -71,9 +114,7 @@ router.get('/login', function(req, res) {
   if (req.user) {
     return res.redirect('/')
   }
-  res.render('user/login', {
-    title: 'login'
-  })
+  res.render('user/login')
 })
 
 
@@ -97,14 +138,14 @@ router.post('/login', function(req, res, next) {
       return next(err)
     }
     if (!user) {
-      req.flash('errors', { msg: info.message })
+      req.flash('errors', { msg: info.msg })
       return res.redirect('/login')
     }
     req.logIn(user, function(err) {
       if (err) {
         return next(err)
       }
-      req.flash('success', { msg: 'Login successed!' })
+      req.flash('success', { msg: '登录成功' })
       return res.redirect(req.session.returnTo || '/')
     })
   })(req, res, next)
@@ -129,7 +170,7 @@ router.get('/account', passportConfig.isAuthenticated, function(req, res) {
   if (!req.user) {
     return res.redirect('/login')
   }
-  res.render('user/profile')
+  res.render('user/profile', { title: 'profile' })
 })
 
 
@@ -151,7 +192,7 @@ router.post('/account/profile', passportConfig.isAuthenticated, function(req, re
       if (err) {
         return next(err)
       }
-      req.flash('success', { msg: 'Profile information has been updated.' })
+      req.flash('success', { msg: '个人信息已更新' })
       return res.redirect('/account')
     })
   })
@@ -163,28 +204,38 @@ router.post('/account/profile', passportConfig.isAuthenticated, function(req, re
  * Update current password.
  */
 router.post('/account/password', passportConfig.isAuthenticated, function(req, res, next) {
-  req.assert('password', 'Password must be at least 8 characters long').len(8)
-  req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password)
-
-  var errors = req.validationErrors()
-
-  if (errors) {
-    req.flash('errors', errors)
-    return res.redirect('/account')
-  }
-
   User.findById(req.user.id, function(err, user) {
     if (err) {
       return next(err)
     }
 
-    user.password = req.body.password
-    user.save(function(err) {
+    user.comparePassword(req.body['old-password'], (err, isMatch) => {
       if (err) {
         return next(err)
       }
-      req.flash('success', { msg: 'Password has been changed.' })
-      res.redirect('/account')
+      if (isMatch === true) {
+        req.assert('password', 'Password must be at least 8 characters long').len(8)
+        req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password)
+
+        var errors = req.validationErrors()
+
+        if (errors) {
+          req.flash('errors', errors)
+          return res.redirect('/account')
+        }
+
+        user.password = req.body.password
+        user.save(function(err) {
+          if (err) {
+            return next(err)
+          }
+          req.flash('success', { msg: '密码已更改' })
+          res.redirect('/account')
+        })
+      } else {
+        req.flash('errors', { msg: '旧密码错误' })
+        return res.redirect('/account')
+      }
     })
   })
 })
@@ -200,11 +251,16 @@ router.post('/account/delete', passportConfig.isAuthenticated, function(req, res
       return next(err)
     }
     req.logout()
-    req.flash('info', { msg: 'Your account has been deleted.' })
+    req.flash('info', { msg: '账户已被删除' })
     return res.redirect('/')
   })
 })
 
+
+/**
+ * GET /account/unlink/:provider
+ * Unlink third party account.
+ */
 router.get('/account/unlink/:provider', passportConfig.isAuthenticated, function(req, res, next) {
   const provider = req.params.provider
   User.findById(req.user.id, (err, user) => {
@@ -220,7 +276,7 @@ router.get('/account/unlink/:provider', passportConfig.isAuthenticated, function
       if (provider == 'github') {
         var provider_name = 'GitHub'
       }
-      req.flash('info', { msg: `${provider_name} account has been unlinked.` })
+      req.flash('info', { msg: `${provider_name} 已被断开连接` })
       return res.redirect('/account')
     })
   })
