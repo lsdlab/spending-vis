4b325f60a795e7ea628dc50b63dc6fd45d918598
Chen Jian <lsdlab@icloud.com>
2016-11-16 22:07:33 +0800

    some fix

--- routes/db.js
+++ routes/db.js
@@ -1,15 +1,9 @@
 require('dotenv').config()
 const chalk = require('chalk')
-// Proper way to initialize and share the Database object
-// Loading and initializing the library:
-const pgp = require('pg-promise')({
-    // Initialization Options
-})
+const pgp = require('pg-promise')()
 
-// Preparing the connection details:
 const cn = process.env.POSTGRESQL_URL
 
-// Creating a new database instance from the connection details:
 const db = pgp(cn)
 
 console.log('%s PostgreSQL connection established!', chalk.blue('✓'))
